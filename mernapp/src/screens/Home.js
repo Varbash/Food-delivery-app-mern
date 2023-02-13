a99eeea119460667b7f7b3e1aa7fb06c7c345880
VARUNB19302 <varbash1419@gmail.com>
2023-02-13 20:27:22 +0530

    creating react app and navbar

---
+++ mernapp/src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+export default function Home() {
+  return (
+    <div>
+        <div> <Navbar/></div>
+        <div>hello</div>
+        <div><Footer/></div>
+    </div>
+  )
+}

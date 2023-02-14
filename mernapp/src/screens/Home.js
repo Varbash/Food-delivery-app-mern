96139dd10594ea35908c068fd8d6cabc145183b8
VARUNB19302 <varbash1419@gmail.com>
2023-02-14 20:01:17 +0530

    continue

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div>
-        <div> <Navbar/></div>
-        <div>hello</div>
-        <div><Footer/></div>
+      <div>
+        {" "}
+        <Navbar />
+      </div>
+
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }

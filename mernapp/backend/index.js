0f21452d7ca4a8b3e369bed3fb89f41c0d400ecb
VARUNB19302 <varbash1419@gmail.com>
2023-03-03 12:34:04 +0530

    changes

--- mernapp/backend/index.js
+++ mernapp/backend/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoDB = require('./db')
-mongoDB();
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use(express.json())
+app.use('/api',require("./Routes/CreateUser"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })

ab47942c5770f3dd49d406bd00e5c0d035b3a3d8
VARUNB19302 <varbash1419@gmail.com>
2023-03-03 11:38:18 +0530

    updates

--- mernapp/backend/db.js
+++ mernapp/backend/db.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
-const mongoURI='mongodb+srv://varbash1419:Yash@401@cluster0.syouqe8.mongodb.net/?retryWrites=true&w=majority'
-const mongoDB =() =>{
-  mongoose.connect(mongoURI,()=>
+const mongoURI='mongodb+srv://Varun:Varun123@cluster0.rnzjngp.mongodb.net/MERN?retryWrites=true&w=majority'
+const mongoDB =async() =>{
+  await mongoose.connect(mongoURI,{ useNewUrlParser:true},async(err,result)=>{
+    if(err) console.log("----",err)
+    else
 {
     console.log('Connected to MongoDB');
-});
+    const fetched_data= await mongoose.connection.db.collection("food_category");
+    fetched_data.find({}).toArray(function(err, data){
+    if (err) console.log(err);
+    else console.log(data)
+    })
+};
 
+})
 }
-
-module.exports = mongoDB;
+module.exports = mongoDB();
 
 
